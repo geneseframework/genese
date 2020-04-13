62777f065f29ee5d2166603766e035b6281bd1e3
Gilles Fabre <geneseframework@gmail.com>
2020-04-13 11:03:46 +0200

    Cli runs with index.ts

--- index-old.js
+++ index-old.js
@@ -1,19 +1,28 @@
 #!/usr/bin/env node
-"use strict";
-exports.__esModule = true;
-var program = require('commander').program;
+const { program } = require('commander');
+
 console.log("Starts Genese cli");
-var zzz = __dirname;
+
+const zzz = __dirname;
 console.log("zzz ", zzz);
+
 program.version('0.0.1')
     .description('Genese cli');
+
 program.option('-h, --help', 'Help genese CLI')
     .option('-d, --debug', 'Debugging');
+
 program.command('new <type>')
     .description('New app | api')
-    .action(function (context) {
-    console.log('The context is : ', context);
-});
+    .action((context) => {
+        console.log('The context is : ', context);
+    });
+
 program.parse(process.argv);
-exports.PROJECT_DIR = 'aaa';
+
+export const PROJECT_DIR = 'aaa';
+
 console.log("End of Genese cli");
+
+
+
