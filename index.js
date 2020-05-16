e39a51b74c0669d8de4887672bd06f7021ac7527
Gilles Fabre <gillesfabrejob@gmail.com>
2020-05-16 15:11:08 +0200

    Copy file

--- index.js
+++ index.js
@@ -1,34 +1,45 @@
 #!/usr/bin/env node
 "use strict";
 exports.__esModule = true;
+require("./");
 var ansi_colors_1 = require("ansi-colors");
-var child_process_1 = require("child_process");
+var fs = require("fs-extra");
 var program = require('commander').program;
 console.log(ansi_colors_1.blueBright("Starts Genese cli"));
-exports.GENESE_DIR = __dirname;
+console.log('INDEX.JS');
+console.log('DIRNAME', __dirname);
 exports.PROJECT_DIR = process.cwd();
-try {
-    program.version('0.0.4')
-        .description('Genese cli');
-    program.command('new <type>')
-        .description('New app | api')
-        .action(function () {
-        var pathIndex = "node " + exports.PROJECT_DIR + "/node_modules/genese-api-angular/index.js";
-        child_process_1.exec(pathIndex, function (error, stdout, stderr) {
-            if (error) {
-                console.log(ansi_colors_1.red("Error in Genese cli execution : " + error.message));
-                return;
-            }
-            if (stderr) {
-                console.log(ansi_colors_1.red("Error in Genese cli command : " + stderr));
-                return;
-            }
-            console.log(ansi_colors_1.yellowBright("" + stdout));
-            console.log(ansi_colors_1.blueBright("Genese cli created genese API successfully."));
-        });
-    });
-    program.parse(process.argv);
-}
-catch (err) {
-    console.error(ansi_colors_1.red("Error in Genese cli process : " + err));
-}
+console.log('PROJECT_DIR', exports.PROJECT_DIR);
+fs.copyFileSync(__dirname + '/LICENSE', exports.PROJECT_DIR + '/COPY');
+// try {
+//
+//     program.version('0.0.4')
+//         .description('Genese cli');
+//
+//     program.command('new <type>')
+//         .description('New app | api')
+//         .action(() => {
+//             const pathIndex = `node ${PROJECT_DIR}/node_modules/genese-api-angular/index.js`;
+//             exec(pathIndex, (error, stdout, stderr) => {
+//                 if (error) {
+//                     console.log(red(`Error in Genese cli execution : ${error.message}`));
+//                     return;
+//                 }
+//                 if (stderr) {
+//                     console.log(red(`Error in Genese cli command : ${stderr}`));
+//                     return;
+//                 }
+//                 console.log(yellowBright(`${stdout}`));
+//                 console.log(blueBright("Genese cli created genese API successfully."));
+//             });
+//         });
+//
+//     program.parse(process.argv);
+//
+// } catch (err) {
+//     console.error(red(`Error in Genese cli process : ${err}`));
+// }
+//
+//
+//
+//
--- index.ts
+++ index.ts
@@ -1,43 +1,49 @@
 #!/usr/bin/env node
+
+require("./");
 import { blueBright, red, yellowBright } from 'ansi-colors';
 import { exec } from 'child_process';
+import * as fs from 'fs-extra';
 
 const { program } = require('commander');
 
 console.log(blueBright("Starts Genese cli"));
 
-export const GENESE_DIR = __dirname;
+console.log('INDEX.JS')
+console.log('DIRNAME', __dirname);
 export const PROJECT_DIR = process.cwd();
 
-try {
-
-    program.version('0.0.4')
-        .description('Genese cli');
-
-    program.command('new <type>')
-        .description('New app | api')
-        .action(() => {
-            const pathIndex = `node ${PROJECT_DIR}/node_modules/genese-api-angular/index.js`;
-            exec(pathIndex, (error, stdout, stderr) => {
-                if (error) {
-                    console.log(red(`Error in Genese cli execution : ${error.message}`));
-                    return;
-                }
-                if (stderr) {
-                    console.log(red(`Error in Genese cli command : ${stderr}`));
-                    return;
-                }
-                console.log(yellowBright(`${stdout}`));
-                console.log(blueBright("Genese cli created genese API successfully."));
-            });
-        });
-
-    program.parse(process.argv);
-
-} catch (err) {
-    console.error(red(`Error in Genese cli process : ${err}`));
-}
-
-
-
-
+console.log('PROJECT_DIR', PROJECT_DIR);
+fs.copyFileSync(__dirname + '/LICENSE', PROJECT_DIR + '/COPY');
+// try {
+//
+//     program.version('0.0.4')
+//         .description('Genese cli');
+//
+//     program.command('new <type>')
+//         .description('New app | api')
+//         .action(() => {
+//             const pathIndex = `node ${PROJECT_DIR}/node_modules/genese-api-angular/index.js`;
+//             exec(pathIndex, (error, stdout, stderr) => {
+//                 if (error) {
+//                     console.log(red(`Error in Genese cli execution : ${error.message}`));
+//                     return;
+//                 }
+//                 if (stderr) {
+//                     console.log(red(`Error in Genese cli command : ${stderr}`));
+//                     return;
+//                 }
+//                 console.log(yellowBright(`${stdout}`));
+//                 console.log(blueBright("Genese cli created genese API successfully."));
+//             });
+//         });
+//
+//     program.parse(process.argv);
+//
+// } catch (err) {
+//     console.error(red(`Error in Genese cli process : ${err}`));
+// }
+//
+//
+//
+//
