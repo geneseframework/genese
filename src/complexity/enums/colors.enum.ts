433593f14b90e7cd0019f7258b2b0da5a212cf0c
whanegen <albert.grisvard@altran.com>
2020-06-11 10:53:58 +0200

    change color

--- src/complexity/enums/colors.enum.ts
+++ src/complexity/enums/colors.enum.ts
@@ -2,9 +2,9 @@
  * Colors of the different charts
  */
 export enum ChartColor {
-    CORRECT = 'rgba(184, 233, 148,1.0)',
-    WARNING = 'rgba(246, 185, 59,1.0)',
-    ERROR = 'rgba(229, 80, 57,1.0)'
+    CORRECT = '#99ccff',
+    WARNING = '#ffa31a',
+    ERROR = '#ff3300'
 }
 
 
