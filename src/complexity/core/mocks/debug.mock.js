44f72cf064caea14c8a491f0c510d98bdb6ba92a
Gilles Fabre <gillesfabrejob@gmail.com>
2020-07-03 09:04:30 +0200

    Updated README

--- src/complexity/core/mocks/debug.mock.js
+++ src/complexity/core/mocks/debug.mock.js
@@ -7,6 +7,7 @@ class DebugMock {
      */
     ngOnInit() {
         // ---
+        let a = 3;
         setTimeout(() => this.opened = true);
         this.setVersionSelected();
     }
--- src/complexity/core/mocks/debug.mock.ts
+++ src/complexity/core/mocks/debug.mock.ts
@@ -12,6 +12,7 @@ export class DebugMock  {
      */
     ngOnInit() {
         // ---
+        let a = 3;
         setTimeout(() => this.opened = true);
         this.setVersionSelected();
     }
