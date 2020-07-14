00421bed7de62541acce937658a7a90e8de14bdc
Gilles Fabre <gillesfabrejob@gmail.com>
2020-07-14 11:18:26 +0200

    Added IdentifierType

--- src/complexity/core/interfaces/ast/ast-node.interface.ts
+++ src/complexity/core/interfaces/ast/ast-node.interface.ts
@@ -1,12 +1,16 @@
 import { SyntaxKind } from '../../enum/syntax-kind.enum';
+import { CpxFactors } from '../../models/cpx-factor/cpx-factors.model';
+import { IdentifierType } from '../identifier-type.type';
 
 export interface AstNodeInterface {
 
     children?: AstNodeInterface[];
+    cpxFactors?: CpxFactors;
     end: number;
     kind: SyntaxKind;
     name?: string;
     pos: number;
     start?: number;
+    type?: IdentifierType;
 
 }
