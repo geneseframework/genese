963d49061ac4db6217a1cad3e007bcb83ef4412a
Genese <63037808+geneseframework@users.noreply.github.com>
2020-07-26 12:23:10 +0200

    Removed lodash (#81)
    Co-authored-by: Gilles Fabre <gillesfabrejob@gmail.com>

--- src/complexity/core/mocks/debug.mock.ts
+++ src/complexity/core/mocks/debug.mock.ts
@@ -1,12 +1,8 @@
-import * as _ from 'lodash';
-import { CallbacksMock } from './mocks-subfolder/callbacks.mock';
 
 export class DebugMock  {
 
     method(a: string) {
         const b: number[] = [1, 2];
-        const c = _.tail(b);
-        console.log('C = ', c.toString())
         const d = b.reduce(() => {
                     return;
                 }, undefined)
