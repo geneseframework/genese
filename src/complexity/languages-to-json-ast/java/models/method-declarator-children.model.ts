36c566f629ceb523fa54a1dc79545baedc260bca
Bachir GUENDOUZ <bachir.guendouz@altran.com>
2020-08-31 11:02:22 +0200

    correction code review

--- src/complexity/languages-to-json-ast/java/models/method-declarator-children.model.ts
+++ src/complexity/languages-to-json-ast/java/models/method-declarator-children.model.ts
@@ -2,8 +2,8 @@ import { Infos } from "./infos.model";
 import { FormalParameterList } from "./formal-parameter-list.model";
 
 export class MethodDeclaratorChildren {
-    Identifier?: Infos[] = [new Infos()];
-    LBrace?: Infos[] = [new Infos()];
+    identifier?: Infos[] = [new Infos()];
+    lBrace?: Infos[] = [new Infos()];
     formalParameterList?: FormalParameterList[] = [new FormalParameterList()];
-    RBrace?: Infos[] = [new Infos()];
+    rBrace?: Infos[] = [new Infos()];
 }
