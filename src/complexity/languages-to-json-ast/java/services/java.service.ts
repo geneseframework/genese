1b5dfd103422394d63e124a13d0b02f2fcc5164d
Bachir GUENDOUZ <bachir.guendouz@altran.com>
2020-09-01 16:12:00 +0200

    clean code

--- src/complexity/languages-to-json-ast/java/services/java.service.ts
+++ src/complexity/languages-to-json-ast/java/services/java.service.ts
@@ -10,27 +10,6 @@ import { AnnotationChildren } from '../models/annotation-children.model';
 export class JavaService {
 
     /**
-     * @param  {Infos} infos
-     * @param  {AstNodeInterface} astNode
-     * @returns AstNodeInterface
-     */
-    static getAstNodeInfos(infos: Infos[], astNode: AstNodeInterface): AstNodeInterface {
-        if(infos) {
-            infos.forEach(info => {
-                let childrenAstNode: AstNodeInterface = {
-                    end: info.endOffset,
-                    kind: info.image,
-                    name: info.image,
-                    pos: info.startOffset,
-                    start: info.startOffset
-                }
-                astNode.children.push(childrenAstNode);
-            });
-        }
-        return astNode;
-    }
-
-    /**
      * Gets astNode with children
      * @param node // AST Node
      */
@@ -43,33 +22,7 @@ export class JavaService {
             start: node.location.startOffset
         }
         astNode.children = [];
-
         return astNode;
     }
-    
-    /**
-     * @param  {Annotation[]} annotations
-     * @param  {AstNodeInterface} annotationAstNode
-     * @returns AstNodeInterface
-     */
-    static generateAstAnnotation(annotations: Annotation[], annotationAstNode: AstNodeInterface): AstNodeInterface {
-        annotations.forEach(annotation => {
-            let astNode = JavaService.getAstNodeWithChildren(annotation);
-            JavaService.generateAstAnnotationChildren(annotation.children, astNode);
-            annotationAstNode.children.push(astNode);
-        })
-        return annotationAstNode;
-    }
 
-    /**
-     * @param  {AnnotationChildren} annotationChildren
-     * @param  {AstNodeInterface} astNode
-     * @returns void
-     */
-    static generateAstAnnotationChildren(annotationChildren: AnnotationChildren, astNode: AstNodeInterface): void {
-        if(annotationChildren) {
-            JavaService.getAstNodeInfos(annotationChildren.at, astNode);
-            JavaService.getAstNodeInfos(annotationChildren.typeName, astNode);
-        } 
-    }
 }
