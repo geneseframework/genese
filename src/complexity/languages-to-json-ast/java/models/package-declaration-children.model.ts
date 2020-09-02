8c46f3f0d5ad0cc86380207570fe3e53551fd699
Bachir GUENDOUZ <bachir.guendouz@altran.com>
2020-09-02 14:49:28 +0200

    comment for PascalCase properties

--- src/complexity/languages-to-json-ast/java/models/package-declaration-children.model.ts
+++ src/complexity/languages-to-json-ast/java/models/package-declaration-children.model.ts
@@ -1,5 +1,9 @@
 import { Infos } from "./infos.model";
 
+/**
+ * We are forced to use PascalCase for the properties
+ * java-parser returns PascalCase properties 
+ */
 export class PackageDeclarationChildren {
     Dot?: Infos[] = [new Infos()];
     Identifier?: Infos[] = [new Infos()];
