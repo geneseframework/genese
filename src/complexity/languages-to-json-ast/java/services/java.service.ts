df93871e57f4c220d87650f35785ca053420e934
Bachir GUENDOUZ <bachir.guendouz@altran.com>
2020-09-02 16:19:24 +0200

    import service

--- src/complexity/languages-to-json-ast/java/services/java.service.ts
+++ src/complexity/languages-to-json-ast/java/services/java.service.ts
@@ -1,5 +1,6 @@
 import { AstNodeInterface } from '../../../core/interfaces/ast/ast-node.interface';
 import { Infos } from '../models/infos.model';
+import { Node } from '../models/node.model';
 import { Annotation } from '../models/annotation.model';
 import { TypeIdentifier } from '../models/type-identifier.model';
 import { AnnotationChildren } from '../models/annotation-children.model';
@@ -13,15 +14,42 @@ export class JavaService {
      * Gets astNode with children
      * @param node // AST Node
      */
-    static getAstNodeWithChildren(node): AstNodeInterface{
-        let astNode: AstNodeInterface = {
-            end: node.location.endOffset,
-            kind: node.name,
-            name: node.name,
-            pos: node.location.startOffset,
-            start: node.location.startOffset
+    static getAstNodeWithChildren(node: Node): AstNodeInterface {
+        let astNode: AstNodeInterface;
+        if(node?.location){
+            astNode = {
+                end: node.location.endOffset,
+                kind: node.name,
+                name: node.name,
+                pos: node.location.startOffset,
+                start: node.location.startOffset
+            }
         }
         astNode.children = [];
+        return astNode;
+    }
+
+
+    /**
+     * @param  {Infos} infos
+     * @param  {AstNodeInterface} astNode
+     * @returns AstNodeInterface
+     */
+    static getAstNodeInfos(infos: Infos[], astNode: AstNodeInterface): AstNodeInterface {
+        if(infos) {
+            infos.forEach(info => {
+                let childrenAstNode: AstNodeInterface = {
+                    end: info.endOffset,
+                    kind: info.image,
+                    name: info.image,
+                    pos: info.startOffset,
+                    start: info.startOffset
+                }
+                if(astNode?.children){
+                    astNode.children.push(childrenAstNode);
+                }
+            });
+        }
         return astNode;
     }
 
