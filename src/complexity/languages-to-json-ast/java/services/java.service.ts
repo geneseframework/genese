c903eb5366efd4f6a4cd6c266586bcc33e6444fb
guendouzbachir <guendouzbachir@hotmail.com>
2020-09-03 14:06:28 +0200

    package service (#87)
    * package service
    
    * comment for PascalCase properties
    
    * refacto + adding space
    
    * corrections retour
    
    * corrections retour
    
    Co-authored-by: Bachir GUENDOUZ <bachir.guendouz@altran.com>

--- src/complexity/languages-to-json-ast/java/services/java.service.ts
+++ src/complexity/languages-to-json-ast/java/services/java.service.ts
@@ -1,4 +1,5 @@
 import { AstNodeInterface } from '../../../core/interfaces/ast/ast-node.interface';
+import { Node } from '../models/node.model';
 import { Infos } from '../models/infos.model';
 import { Annotation } from '../models/annotation.model';
 import { TypeIdentifier } from '../models/type-identifier.model';
@@ -8,20 +9,48 @@ import { AnnotationChildren } from '../models/annotation-children.model';
  * Service contains common functions
  */
 export class JavaService {
-
+    
     /**
      * Gets astNode with children
-     * @param node // AST Node
+     * @param  {Node} node
+     * @returns AstNodeInterface
      */
-    static getAstNodeWithChildren(node): AstNodeInterface{
-        let astNode: AstNodeInterface = {
-            end: node.location.endOffset,
-            kind: node.name,
-            name: node.name,
-            pos: node.location.startOffset,
-            start: node.location.startOffset
+    static getAstNodeWithChildren(node: Node): AstNodeInterface {
+        let astNode: AstNodeInterface;
+        if(node?.location){
+            astNode = {
+                end: node.location.endOffset,
+                kind: node.name,
+                name: node.name,
+                pos: node.location.startOffset,
+                start: node.location.startOffset
+            }
         }
         astNode.children = [];
+        return astNode;
+    }
+
+    
+    /**
+     * @param  {Infos[]} infos
+     * @param  {AstNodeInterface} astNode
+     * @returns AstNodeInterface
+     */
+    static getAstNodeInfos(infos: Infos[], astNode: AstNodeInterface): AstNodeInterface {
+        if(Array.isArray(infos)) {
+            infos.forEach(info => {
+                let childrenAstNode: AstNodeInterface = {
+                    end: info.endOffset,
+                    kind: info.image,
+                    name: info.image,
+                    pos: info.startOffset,
+                    start: info.startOffset
+                }
+                if(astNode?.children){
+                    astNode.children.push(childrenAstNode);
+                }
+            });
+        }
         return astNode;
     }
 
