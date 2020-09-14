3743db6c6326070f8a00db2e72dde4aa0e81b016
guendouzbachir <guendouzbachir@hotmail.com>
2020-09-14 15:04:04 +0200

    Feature java parser switch (#96)
    * switch model
    
    * switch parser methods
    
    Co-authored-by: Bachir GUENDOUZ <bachir.guendouz@altran.com>

--- src/complexity/languages-to-json-ast/java/models/binary.expression.children.model.ts
+++ src/complexity/languages-to-json-ast/java/models/binary.expression.children.model.ts
@@ -1,7 +1,10 @@
 import { Infos } from "./infos.model";
 import { UnaryExpression } from "./unary.expression.model";
+import { Expression } from "./expression.model";
 
 export class BinaryExpressionChildren {
     unaryExpression?: UnaryExpression[] = [new UnaryExpression()];
     BinaryOperator?: Infos[] = [new Infos()];
+    AssignmentOperator?: Infos[] = [new Infos()];
+    expression?: Expression[] = [new Expression()];
 }
