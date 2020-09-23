a66a7debd2fb28a96d8d0160a43a1aac9531bd3e
Alexandre Kueny <alexandre.kueny@altran.com>
2020-09-23 14:03:22 +0200

    analyse methodDeclaration

---
+++ src/complexity/languages-to-json-ast/java/cstToAstCases/formal-parameter-list.js
@@ -0,0 +1,17 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.run = void 0;
+const cstToAst_1 = require("../cstToAst");
+// @ts-ignore
+function run(cstNode, children) {
+    const formalParameter = children.formalParameter;
+    return {
+        kind: 'FormalParameterList',
+        start: cstNode.location.startOffset,
+        end: cstNode.location.endOffset,
+        children: [
+            ...formalParameter.map(e => cstToAst_1.cstToAst(e))
+        ]
+    };
+}
+exports.run = run;
--- src/complexity/languages-to-json-ast/java/cstToAstCases/statement.js
+++ src/complexity/languages-to-json-ast/java/cstToAstCases/statement.js
@@ -5,8 +5,11 @@ const cstToAst_1 = require("../cstToAst");
 // @ts-ignore
 function run(cstNode, children) {
     var _a, _b;
+    const statementWithoutTrailingSubstatement = children.statementWithoutTrailingSubstatement;
+    const ifStatement = children.ifStatement;
     return [
-        ...[].concat(...(_b = (_a = children.statementWithoutTrailingSubstatement) === null || _a === void 0 ? void 0 : _a.map(e => cstToAst_1.cstToAst(e))) !== null && _b !== void 0 ? _b : [])
+        ...[].concat(...(_a = statementWithoutTrailingSubstatement === null || statementWithoutTrailingSubstatement === void 0 ? void 0 : statementWithoutTrailingSubstatement.map(e => cstToAst_1.cstToAst(e))) !== null && _a !== void 0 ? _a : []),
+        ...[].concat(...(_b = ifStatement === null || ifStatement === void 0 ? void 0 : ifStatement.map(e => cstToAst_1.cstToAst(e))) !== null && _b !== void 0 ? _b : [])
     ];
 }
 exports.run = run;
