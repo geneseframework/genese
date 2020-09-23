5525723621a51863d395d2da8737b886c3120d5a
Alexandre Kueny <alexandre.kueny@altran.com>
2020-09-23 17:16:27 +0200

    fixed binaryExpression

--- src/complexity/languages-to-json-ast/java/cstToAstCases/binary-expression.js
+++ src/complexity/languages-to-json-ast/java/cstToAstCases/binary-expression.js
@@ -4,25 +4,24 @@ exports.run = void 0;
 const cstToAst_1 = require("../cstToAst");
 // @ts-ignore
 function run(cstNode, children) {
-    var _a;
     const unaryExpressions = children.unaryExpression;
-    const binaryOperator = (_a = children.BinaryOperator) === null || _a === void 0 ? void 0 : _a[0];
+    const binaryOperators = children.BinaryOperator;
     const unaryExpressionsAst = unaryExpressions.map(e => cstToAst_1.cstToAst(e));
-    if (binaryOperator) {
+    if (binaryOperators) {
+        const binaryOperatorsAst = binaryOperators.map(e => cstToAst_1.cstToAst(e, 'binaryOperator'));
         return {
             kind: 'BinaryExpression',
             start: cstNode.location.startOffset,
             end: cstNode.location.endOffset,
             pos: cstNode.location.startOffset,
             children: [
-                unaryExpressionsAst[0],
-                {
-                    kind: cstToAst_1.getBinaryOperatorName(binaryOperator.image),
-                    start: binaryOperator.startOffset,
-                    end: binaryOperator.endOffset,
-                },
-                unaryExpressionsAst[1]
-            ]
+                ...[].concat(...unaryExpressionsAst.map((unaryExp, i) => {
+                    return [
+                        unaryExp,
+                        binaryOperatorsAst === null || binaryOperatorsAst === void 0 ? void 0 : binaryOperatorsAst[i],
+                    ];
+                }))
+            ].filter(e => e)
         };
     }
     else {
--- src/complexity/languages-to-json-ast/java/cstToAstCases/binary-expression.ts
+++ src/complexity/languages-to-json-ast/java/cstToAstCases/binary-expression.ts
@@ -3,25 +3,25 @@ import { cstToAst, getBinaryOperatorName } from '../cstToAst';
 // @ts-ignore
 export function run(cstNode, children) {
     const unaryExpressions = children.unaryExpression;
-    const binaryOperator = children.BinaryOperator?.[0];
+    const binaryOperators = children.BinaryOperator;
 
     const unaryExpressionsAst = unaryExpressions.map(e => cstToAst(e));
 
-    if (binaryOperator) {
+    if (binaryOperators) {
+        const binaryOperatorsAst = binaryOperators.map(e => cstToAst(e, 'binaryOperator'));
         return {
             kind: 'BinaryExpression',
             start: cstNode.location.startOffset,
             end: cstNode.location.endOffset,
             pos: cstNode.location.startOffset,
             children: [
-                unaryExpressionsAst[0],
-                {
-                    kind: getBinaryOperatorName(binaryOperator.image),
-                    start: binaryOperator.startOffset,
-                    end: binaryOperator.endOffset,
-                },
-                unaryExpressionsAst[1]
-            ]
+                ...[].concat(...unaryExpressionsAst.map((unaryExp, i) => {
+                    return [
+                        unaryExp,
+                        binaryOperatorsAst?.[i],
+                    ]
+                }))
+            ].filter(e => e)
         };
     } else {
         return unaryExpressionsAst[0];
