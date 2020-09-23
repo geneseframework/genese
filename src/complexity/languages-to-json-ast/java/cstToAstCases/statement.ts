66d45b59cc1c57a4ad0d3903b48df9839d1ca739
Alexandre Kueny <alexandre.kueny@altran.com>
2020-09-23 14:03:22 +0200

    analyse methodDeclaration

--- src/complexity/languages-to-json-ast/java/cstToAstCases/statement.ts
+++ src/complexity/languages-to-json-ast/java/cstToAstCases/statement.ts
@@ -2,7 +2,11 @@ import { cstToAst } from '../cstToAst';
 
 // @ts-ignore
 export function run(cstNode, children) {
+    const statementWithoutTrailingSubstatement = children.statementWithoutTrailingSubstatement;
+    const ifStatement = children.ifStatement;
+
     return [
-        ...[].concat(...children.statementWithoutTrailingSubstatement?.map(e => cstToAst(e)) ?? [])
+        ...[].concat(...statementWithoutTrailingSubstatement?.map(e => cstToAst(e)) ?? []),
+        ...[].concat(...ifStatement?.map(e => cstToAst(e)) ?? [])
     ];
 }
