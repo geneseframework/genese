ac663c4ff89b519a288afedaf958e2f5f89c1795
Alexandre Kueny <alexandre.kueny@altran.com>
2020-09-24 09:44:43 +0200

    parenthesisExpression

--- src/complexity/languages-to-json-ast/java/cstToAstCases/unary-expression.js
+++ src/complexity/languages-to-json-ast/java/cstToAstCases/unary-expression.js
@@ -5,14 +5,27 @@ const cstToAst_1 = require("../cstToAst");
 // @ts-ignore
 function run(cstNode, children) {
     const unaryPrefixOperator = children.UnaryPrefixOperator;
-    const identifier = children.primary[0].children.primaryPrefix[0].children.fqnOrRefType[0].children.fqnOrRefTypePartFirst[0].children.fqnOrRefTypePartCommon[0].children.Identifier[0];
-    if (unaryPrefixOperator) {
-        const unaryPrefixOperatorAst = cstToAst_1.cstToAst(unaryPrefixOperator, 'unaryPrefixOperator');
-        unaryPrefixOperatorAst.children.push(cstToAst_1.cstToAst(identifier, 'identifier'));
-        return unaryPrefixOperatorAst;
-    }
-    else {
-        return cstToAst_1.cstToAst(identifier, 'identifier');
-    }
+    const primary = children.primary;
+    return [
+        // cstToAst(unaryPrefixOperator, 'unaryPrefixOperator'),
+        ...[].concat(...primary.map(e => cstToAst_1.cstToAst(e)))
+    ];
+    // return {
+    //     kind: 'UnaryExpression',
+    //     start: cstNode.location.startOffset,
+    //     end: cstNode.location.endOffset,
+    //     pos: cstNode.location.startOffset,
+    //     children: [
+    //         // cstToAst(unaryPrefixOperator, 'unaryPrefixOperator'),
+    //         ...[].concat(...primary.map(e => cstToAst(e)))
+    //     ]
+    // };
+    // if (unaryPrefixOperator) {
+    //     const unaryPrefixOperatorAst = cstToAst(unaryPrefixOperator, 'unaryPrefixOperator');
+    //     unaryPrefixOperatorAst.children.push(cstToAst(identifier, 'identifier'));
+    //     return unaryPrefixOperatorAst;
+    // } else {
+    //     return cstToAst(identifier, 'identifier');
+    // }
 }
 exports.run = run;
