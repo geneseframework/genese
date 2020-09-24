9b255d9886f04fdd84224366b721f2c894011c12
Alexandre Kueny <alexandre.kueny@altran.com>
2020-09-24 16:59:38 +0200

    added method name to methodDeclaration

--- src/complexity/languages-to-json-ast/java/cstToAstCases/method-declaration.ts
+++ src/complexity/languages-to-json-ast/java/cstToAstCases/method-declaration.ts
@@ -7,14 +7,21 @@ export function run(cstNode: MethodDeclaration, children: MethodDeclarationChild
     const methodHeader = children.methodHeader;
     const methodBody = children.methodBody;
 
+    const methodHeaderAst = methodHeader.map(e => cstToAst(e));
+
     return {
         kind: 'MethodDeclaration',
         start: cstNode.location.startOffset,
         end: cstNode.location.endOffset + 1,
         pos: cstNode.location.startOffset,
+        name: getName(methodHeaderAst),
         children: [
-            ...[].concat(...methodHeader.map(e => cstToAst(e))),
+            ...[].concat(...methodHeaderAst),
             ...[].concat(...methodBody.map(e => cstToAst(e))),
         ]
     };
+}
+
+function getName(methodHeaderAst): string {
+    return methodHeaderAst?.[0]?.[1]?.name ?? ''
 }
