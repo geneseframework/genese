1c3d7a8f281c3dedf58bc26b7a862d3158e39e51
Alexandre Kueny <alexandre.kueny@altran.com>
2020-09-28 11:55:21 +0200

    method call

--- toAnalyse/test.java
+++ toAnalyse/test.java
@@ -1,27 +1,6 @@
 public class Application {
     int somme(int a, int b) {
-        if(a < b) {
-          return a * b;
-        } else {
-            if (!a) {
-                return a + b;
-            }
-            return a - b;
-        }
-    }
-
-    double somme_2(double a, double b, double c) {
-        int e = a + b;
-        b = a + 3;
-        if (a > c) {
-            return a + b * c * 2;
-        }
-        while (!b) {
-            a = a + b;
-        }
-        do {
-            a = a + b;
-        } while (a && !b);
+        this.somme(a, b);
     }
 
     int tryCatchSomme(int a, int b) {
