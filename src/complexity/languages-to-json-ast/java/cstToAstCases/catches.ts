df53a8ae926928add2ecca9c1da646fd9252096e
Gmartin <gabriel.martin@altran.com>
2020-09-29 14:22:14 +0200

    cleaned code

--- src/complexity/languages-to-json-ast/java/cstToAstCases/catches.ts
+++ src/complexity/languages-to-json-ast/java/cstToAstCases/catches.ts
@@ -6,20 +6,6 @@ import { CatchesChildren } from "../models/catches-children";
 
 // @ts-ignoree
 export function run(cstNode: Catches, children: CatchesChildren) {
-    // export function run(cstNode: SwitchStatement, children: SwitchStatementChildren) {
-    // const expression = children.expression;
-    // const switchBlock = children.switchBlock;
-
-    // return {
-    //     kind: 'SwitchStatement',
-    //     start: cstNode.location.startOffset,
-    //     end: cstNode.location.endOffset,
-    //     pos: cstNode.location.startOffset,
-    //     children: [
-    //         ...[].concat(...expression.map(e => cstToAst(e)) ?? []),
-    //         ...[].concat(...switchBlock.map(e => cstToAst(e)) ?? [])
-    //     ]
-    // };
     const catchClause = children.catchClause;
     return [
         ...[].concat(...(catchClause.map((e) => cstToAst(e)) ?? [])),
