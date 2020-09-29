af0b4ea72f0607a90bdd4fd2e81219674de4a272
Gmartin <gabriel.martin@altran.com>
2020-09-29 14:20:42 +0200

    adding example

--- toAnalyse/test.java
+++ toAnalyse/test.java
@@ -1,6 +1,40 @@
 public class Application {
     int somme(int a, int b) {
         try {
+        //  Block of code to try
+            System.out.println("Hello IN!");
+        }
+        catch(Exception e) {
+        //  Block of code to handle errors
+            System.out.println("Hello OUT!");
+        }
+        return 1;
+        // if(a < b) {
+        //   return a * b;
+        // } else {
+        //     if (!a) {
+        //         return a + b;
+        //     }
+        //     return a - b;
+        // }
+    }
+
+    double somme_2(double a, double b, double c) {
+        int e = a + b;
+        b = a + 3;
+        if (a > c) {
+            return a + b * c * 2;
+        }
+        while (!b) {
+            a = a + b;
+        }
+        do {
+            a = a + b;
+        } while (a && !b);
+    }
+
+    int tryCatchSomme(int a, int b) {
+        try {
             a = 1;
         }
         catch(Exception e) {
