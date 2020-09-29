fc9252098cd8f1b8dc743a9f172914a8868e0cb4
Gmartin <gabriel.martin@altran.com>
2020-09-29 14:21:09 +0200

    corrected example

--- toAnalyse/test.java
+++ toAnalyse/test.java
@@ -1,22 +1,13 @@
 public class Application {
     int somme(int a, int b) {
-        try {
-        //  Block of code to try
-            System.out.println("Hello IN!");
+        if(a < b) {
+          return a * b;
+        } else {
+            if (!a) {
+                return a + b;
+            }
+            return a - b;
         }
-        catch(Exception e) {
-        //  Block of code to handle errors
-            System.out.println("Hello OUT!");
-        }
-        return 1;
-        // if(a < b) {
-        //   return a * b;
-        // } else {
-        //     if (!a) {
-        //         return a + b;
-        //     }
-        //     return a - b;
-        // }
     }
 
     double somme_2(double a, double b, double c) {
