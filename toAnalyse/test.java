a0b8ea08b431edf1e6ac11f26c09c5d9e5a0b23c
Bachir GUENDOUZ <bachir.guendouz@altran.com>
2020-09-30 14:58:36 +0200

    fix merge

--- toAnalyse/test.java
+++ toAnalyse/test.java
@@ -1,6 +1,27 @@
 public class Application {
     int somme(int a, int b) {
-        this.somme(a, b);
+        if(a < b) {
+          return a * b;
+        } else {
+            if (!a) {
+                return a + b;
+            }
+            return a - b;
+        }
+    }
+
+    double somme_2(double a, double b, double c) {
+        int e = a + b;
+        b = a + 3;
+        if (a > c) {
+            return a + b * c * 2;
+        }
+        while (!b) {
+            a = a + b;
+        }
+        do {
+            a = a + b;
+        } while (a && !b);
     }
 
     int tryCatchSomme(int a, int b) {
