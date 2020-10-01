59e084a655a2bd83c292f0fac08758617e29df68
Alexandre Kueny <alexandre.kueny@hotmail.com>
2020-10-01 10:07:28 +0200

    propery access and method call (#112)
    * working a.b.c()
    
    * working a.[...].z()
    
    * clean
    
    * working a.[...].z
    
    Co-authored-by: Alexandre Kueny <alexandre.kueny@altran.com>

--- src/complexity/languages-to-json-ast/java/cstToAstCases/primary.ts
+++ src/complexity/languages-to-json-ast/java/cstToAstCases/primary.ts
@@ -2,7 +2,6 @@ import { cstToAst } from '../cst-to-ast';
 import { Primary } from '../models/primary.model';
 import { PrimaryChildren } from '../models/primary-children.model';
 
-// @ts-ignore
 export function run(cstNode: Primary, children: PrimaryChildren): any {
     const primaryPrefix = children.primaryPrefix;
     const primarySuffix = children.primarySuffix;
@@ -14,20 +13,14 @@ export function run(cstNode: Primary, children: PrimaryChildren): any {
         if (primaryPrefixAst.find(e => e.kind === 'ThisKeyword')) {
             return {
                 kind: 'CallExpression',
-                    start: cstNode.location.startOffset,
-                    end: cstNode.location.endOffset,
-                    pos: cstNode.location.startOffset,
+                start: cstNode.location.startOffset,
+                end: cstNode.location.endOffset,
+                pos: cstNode.location.startOffset,
                 children: [
-                    {
-                        kind: 'PropertyAccessExpression',
-                        start: primaryPrefixAst.find(e => e.kind === 'ThisKeyword').start,
-                        end: primarySuffixAst.find(e => e.kind === 'Identifier').end,
-                        pos: primaryPrefixAst.find(e => e.kind === 'ThisKeyword').start,
-                        children: [
-                            primaryPrefixAst.find(e => e.kind === 'ThisKeyword'),
-                            {...primarySuffixAst.find(e => e.kind === 'Identifier'), type: 'function'}
-                        ]
-                    },
+                    toPropertyAccessExpression([
+                        primaryPrefixAst.find(e => e.kind === 'ThisKeyword'),
+                        ...primarySuffixAst.filter(e => e.kind === 'Identifier')
+                    ], true),
                     ...primarySuffixAst.find(e => e.kind === 'MethodInvocationSuffix').children
                 ]
             };
@@ -38,14 +31,58 @@ export function run(cstNode: Primary, children: PrimaryChildren): any {
                 end: cstNode.location.endOffset,
                 pos: cstNode.location.startOffset,
                 children: [
-                    {...primarySuffixAst.find(e => e.kind === 'Identifier'), type: 'function'}
+                    toPropertyAccessExpression([
+                        ...primaryPrefixAst.filter(e => e.kind === 'Identifier')
+                    ], true),
+                    ...primarySuffixAst.find(e => e.kind === 'MethodInvocationSuffix').children
                 ]
             };
         }
     } else {
-        return [
-            ...primaryPrefixAst,
-            ...primarySuffixAst
-        ];
+        if (primaryPrefixAst.length === 1) {
+            return [
+                ...primaryPrefixAst,
+                ...primarySuffixAst
+            ];
+        } else {
+            return [
+                toPropertyAccessExpression(primaryPrefixAst),
+                ...primarySuffixAst
+            ];
+        }
+    }
+}
+
+function toPropertyAccessExpression(identifiers: any[], isFunctionCall = false): any {
+    if (!identifiers) return undefined;
+
+    if (identifiers.length === 1) {
+        return identifiers[0];
+    } else {
+        const last = identifiers.pop();
+
+        if (isFunctionCall) {
+            return {
+                kind: 'PropertyAccessExpression',
+                start: identifiers[0].start,
+                end: last.end,
+                pos: identifiers[0].pos,
+                children: [
+                    toPropertyAccessExpression(identifiers),
+                    {...last, type: 'function'}
+                ]
+            };
+        } else {
+            return {
+                kind: 'PropertyAccessExpression',
+                start: identifiers[0].start,
+                end: last.end,
+                pos: identifiers[0].pos,
+                children: [
+                    toPropertyAccessExpression(identifiers),
+                    last
+                ]
+            };
+        }
     }
 }
