a278d5b100fc1dcd5efbe492514214fc68953ff3
unknown <gabrielmartin.etudiant@gmail.com>
2020-10-01 10:47:02 +0200

    hash-map done

--- toAnalyse/test.java
+++ toAnalyse/test.java
@@ -1,26 +1,46 @@
 public class Application {
+    double somme_2(double a, double b, double c) {
+        int e = a + b;
+        b = a + 3;
+        if (a > c) {
+            return a + b * c * 2;
+        }
+        while (!b) {
+            a = a + b;
+        }
+        do {
+            a = a + b;
+        } while (a && !b);
+    }
 
-  int x;  // Create a class attribute
+    int tryCatchSomme(int a, int b) {
+        try {
+            a = 1;
+        }
+        catch(Exception e) {
+            a = 2;
+        }finally{
+            a = 3;
+        }
+        return a;
 
-  // Create a class constructor for the MyClass class
-  public Application(int b) {
-    this.x = b;  // Set the initial value for the class attribute x
-  }
+    }
 
-    //   int test(){
-    //     return 1;
-    // }
+    void testHashMap() {
+        Map<String, Integer> items = new HashMap<>();
+        items.put("A", 10);
+        items.put("B", 20);
+        items.put("C", 30);
+        items.put("D", 40);
+        items.put("E", 50);
+        items.put("F", 60);
 
-    // int test2(){
+        for (Map.Entry<String, Integer> entry : items.entrySet()) {
+            System.out.println("Hello world");
+        }
+    }
 
-    //     try{
-    //         return 2;
-    //     }catch(Exception e){
-    //         return 3;
-    //     }finally{
-    //         return 4;
-    //     }
-    // }
+
 }
 
 
