af928c4a18c147bb0067933a66b0a2bb204aaa70
Alexandre Kueny <alexandre.kueny@hotmail.com>
2020-10-01 10:07:28 +0200

    propery access and method call (#112)
    * working a.b.c()
    
    * working a.[...].z()
    
    * clean
    
    * working a.[...].z
    
    Co-authored-by: Alexandre Kueny <alexandre.kueny@altran.com>

--- toAnalyse/test.java
+++ toAnalyse/test.java
@@ -1,40 +1,7 @@
 public class Application {
-    int somme(int a, int b) {
-        if(a < b) {
-          return a * b;
-        } else {
-            if (!a) {
-                return a + b;
-            }
-            return a - b;
-        }
+    int somme(String[] a) {
+        return a.b.c();
+        return a.b.c;
+        return a && b;
     }
-
-    double somme_2(double a, double b, double c) {
-        int e = a + b;
-        b = a + 3;
-        if (a > c) {
-            return a + b * c * 2;
-        }
-        while (!b) {
-            a = a + b;
-        }
-        do {
-            a = a + b;
-        } while (a && !b);
-    }
-
-    int tryCatchSomme(int a, int b) {
-        try {
-            a = 1;
-        }
-        catch(Exception e) {
-            a = 2;
-        }finally{
-            a = 3;
-        }
-        return a;
-
-    }
-
 }
