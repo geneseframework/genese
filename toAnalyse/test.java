47bf2277b00e30ce2a2262840e96c34de289d374
Gabriel Martin <36530414+gmartinpro@users.noreply.github.com>
2020-10-06 11:50:02 +0200

    fixed "for" bug (#119)
    * corrected for bug
    
    * cleaned
    
    * cleaned last
    
    * cleaned and added a feature to handle future undefined problems
    
    * cleaned
    
    Co-authored-by: Gmartin <gabriel.martin@altran.com>

--- toAnalyse/test.java
+++ toAnalyse/test.java
@@ -1,7 +1,19 @@
 public class Application {
 
- public void addName(String name) {
-    throw new TechnicalException("test");
+ public void somme(String test) {
+   String name;
+    for(let i = 0; i <10; i++){
+      name += "1";
+      if(name == "2"){
+        System.out.println("YOOO");
+            for(let i = 0; i <10; i++){
+            name += "1";
+            if(name == "2"){
+              System.out.println("YOOO");
+            }
+          }
+      }
+    }
   }
 }
 
