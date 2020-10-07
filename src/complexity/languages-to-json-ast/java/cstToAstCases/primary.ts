ee57cd95fa98a8026c2e54cd96a933b5c2e04490
Bachir GUENDOUZ <bachir.guendouz@altran.com>
2020-10-07 13:49:15 +0200

    api stream

--- src/complexity/languages-to-json-ast/java/cstToAstCases/primary.ts
+++ src/complexity/languages-to-json-ast/java/cstToAstCases/primary.ts
@@ -7,89 +7,94 @@ export function run(cstNode: Primary, children: PrimaryChildren): any {
     const primarySuffix = children.primarySuffix;
 
     const primaryPrefixAst = [].concat(...primaryPrefix?.map(e => cstToAst(e)) ?? []);
-    const primarySuffixAst = [].concat(...primarySuffix?.map(e => cstToAst(e)) ?? []);
+    const primarySuffixAstList = [].concat(primarySuffix?.map(e => cstToAst(e)) ?? []);
     
-    const methodInvocationSuffix = primarySuffixAst.find(e => e.kind === 'MethodInvocationSuffix');
-    const classLiteralSuffix = primarySuffixAst.find(e => e.kind === 'ClassLiteralSuffix');
+   
 
-    const thisKeyword = primaryPrefixAst.find(e => e.kind === 'ThisKeyword');
-    const identifierPrefix = primaryPrefixAst.filter(e => e.kind === 'Identifier');
-    const lambdaExpression = methodInvocationSuffix?.children.find(e => e.kind === 'LambdaExpression');
+    if(Array.isArray(primarySuffixAstList)) {
+        primarySuffixAstList.forEach(primarySuffixAst => {
 
-    if (methodInvocationSuffix) {
-        if (thisKeyword) {
-            return {
-                kind: 'CallExpression',
-                start: cstNode.location.startOffset,
-                end: cstNode.location.endOffset,
-                pos: cstNode.location.startOffset,
-                children: [
-                    toPropertyAccessExpression([
-                        thisKeyword,
-                        ...primarySuffixAst.filter(e => e.kind === 'Identifier')
-                    ], true),
-                    ...primarySuffixAst.find(e => e.kind === 'MethodInvocationSuffix').children
-                ]
-            };
-        } else if(lambdaExpression) {
-            return {
-                kind: 'CallExpression',
-                start: cstNode.location.startOffset,
-                end: cstNode.location.endOffset,
-                pos: cstNode.location.startOffset,
-                children: [
-                    {
-                        kind: 'PropertyAccessExpression',
+            const methodInvocationSuffix = primarySuffixAst.find(e => e.kind === 'MethodInvocationSuffix');
+            const thisKeyword = primaryPrefixAst.find(e => e.kind === 'ThisKeyword');
+            const identifierPrefix = primaryPrefixAst.filter(e => e.kind === 'Identifier');
+            const lambdaExpression = methodInvocationSuffix?.children.find(e => e.kind === 'LambdaExpression');
+            
+            if (methodInvocationSuffix) {
+                if (thisKeyword) {
+                    return {
+                        kind: 'CallExpression',
                         start: cstNode.location.startOffset,
                         end: cstNode.location.endOffset,
                         pos: cstNode.location.startOffset,
                         children: [
-                            ...identifierPrefix,
+                            toPropertyAccessExpression([
+                                thisKeyword,
+                                ...primarySuffixAst.filter(e => e.kind === 'Identifier')
+                            ], true),
+                            ...primarySuffixAst.find(e => e.kind === 'MethodInvocationSuffix').children
                         ]
-                    },
-                    {
-                        kind: 'ArrowFunction',
-                        start: lambdaExpression.start,
-                        end: lambdaExpression.end,
-                        pos: lambdaExpression.pos,
+                    };
+                } else if(lambdaExpression) {
+                    return {
+                        kind: 'CallExpression',
+                        start: cstNode.location.startOffset,
+                        end: cstNode.location.endOffset,
+                        pos: cstNode.location.startOffset,
                         children: [
-                            ...lambdaExpression.children.filter(e => e.kind === 'Parameter'),
-                            ...lambdaExpression.children.filter(e => e.kind === 'EqualsGreaterThanToken'),
-                            lambdaExpression.children.find(e => e.kind === 'ArrowFunction').children
-                                .find(e => e.kind === 'Block')
+                            {
+                                kind: 'PropertyAccessExpression',
+                                start: cstNode.location.startOffset,
+                                end: cstNode.location.endOffset,
+                                pos: cstNode.location.startOffset,
+                                children: [
+                                    ...identifierPrefix,
+                                ]
+                            },
+                            {
+                                kind: 'ArrowFunction',
+                                start: lambdaExpression.start,
+                                end: lambdaExpression.end,
+                                pos: lambdaExpression.pos,
+                                children: [
+                                    ...lambdaExpression.children.filter(e => e.kind === 'Parameter'),
+                                    ...lambdaExpression.children.filter(e => e.kind === 'EqualsGreaterThanToken'),
+                                    lambdaExpression.children.find(e => e.kind === 'ArrowFunction').children
+                                        .find(e => e.kind === 'Block')
+                                ]
+                            }
                         ]
-                    }
-                ]
-            };
-        } else {
-            return {
-                kind: 'CallExpression',
-                start: cstNode.location.startOffset,
-                end: cstNode.location.endOffset,
-                pos: cstNode.location.startOffset,
-                children: [
-                    toPropertyAccessExpression([
-                        ...primaryPrefixAst.filter(e => e.kind === 'Identifier'),
-                        ...primarySuffixAst.filter(e => e.kind === 'Identifier')
-                    ], true),
-                    ...primarySuffixAst.filter(e => e.kind === 'ClassLiteralSuffix'),
-                    ...primarySuffixAst.find(e => e.kind === 'MethodInvocationSuffix').children
-                ]
-            };
-        }
-    } else {
-        if (primaryPrefixAst.length <= 1) {
-            return [
-                ...primaryPrefixAst,
-                ...primarySuffixAst
-            ];
-        } else {
-            return [
-                toPropertyAccessExpression(primaryPrefixAst),
-                ...primarySuffixAst
-            ];
-        }
-    }
+                    };
+                } else {
+                    return {
+                        kind: 'CallExpression',
+                        start: cstNode.location.startOffset,
+                        end: cstNode.location.endOffset,
+                        pos: cstNode.location.startOffset,
+                        children: [
+                            toPropertyAccessExpression([
+                                ...primaryPrefixAst.filter(e => e.kind === 'Identifier'),
+                                ...primarySuffixAst.filter(e => e.kind === 'Identifier')
+                            ], true),
+                            ...primarySuffixAst.filter(e => e.kind === 'ClassLiteralSuffix'),
+                            ...primarySuffixAst.find(e => e.kind === 'MethodInvocationSuffix').children
+                        ]
+                    };
+                }
+            }  else {
+                if (primaryPrefixAst.length <= 1) {
+                    return [
+                        ...primaryPrefixAst,
+                        ...primarySuffixAst
+                    ];
+                } else {
+                    return [
+                        toPropertyAccessExpression(primaryPrefixAst),
+                        ...primarySuffixAst
+                    ];
+                }
+            }
+        })
+    } 
 }
 
 function toPropertyAccessExpression(identifiers: any[], isFunctionCall = false): any {
