5aec22c2ad12942fa7c3ca1a20f99ec393403a31
Bachir GUENDOUZ <bachir.guendouz@altran.com>
2020-10-12 11:58:39 +0200

    assert statement

--- src/complexity/languages-to-json-ast/java/cstToAstCases/assert-statement.ts
+++ src/complexity/languages-to-json-ast/java/cstToAstCases/assert-statement.ts
@@ -1,15 +1,49 @@
 import { cstToAst } from '../cst-to-ast';
 import { AssertStatement } from '../models/assert-statement.model';
 import { AssertStatementChildren } from '../models/assert-statement-children.model';
+import { Expression } from '../models/expression.model';
 
 // @ts-ignore
 export function run(cstNode: AssertStatement, children: AssertStatementChildren): any {
-    const assert = children.Assert;
     const expression = children.expression;
     
-    
-    return [
-        ...assert?.map(e => cstToAst(e, 'identifier')) ?? [],
-        ...[].concat(...expression?.map(e => cstToAst(e)) ?? [])
-    ];
+    return {
+        kind: 'Keyword',
+        start: cstNode.location.startOffset,
+        end: cstNode.location.endOffset,
+        pos: cstNode.location.startOffset,
+        children: [
+            ...generateAssertStatement(expression)
+        ]
+    };
+}
+
+/**
+ * Generate Ast nodes for Assert Statement
+ * @param  {Expression[]} expression
+ * @returns any
+ */
+function generateAssertStatement(expression: Expression[]): any{
+    if(Array.isArray(expression)) {
+        return [
+            {
+                kind: 'IfStatement',
+                start: expression[0].location.startOffset,
+                end: expression[0].location.endOffset,
+                pos: expression[0].location.startOffset,
+                children: [
+                    ...[].concat(...[expression[0]]?.map(e => cstToAst(e)) ?? [])
+                ]
+            },
+            {
+                kind: 'ThrowStatement',
+                start: expression[1].location.startOffset,
+                end: expression[1].location.endOffset,
+                pos: expression[1].location.startOffset,
+                children: [
+                    ...[].concat(...[expression[1]]?.map(e => cstToAst(e)) ?? []),
+                ]
+            }
+        ];
+    }
 }
