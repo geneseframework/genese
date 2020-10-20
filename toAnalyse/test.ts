6e381d6dca81226140deff325b05c24490b2ff4d
Alexandre Kueny <alexandre.kueny@altran.com>
2020-10-20 16:24:01 +0200

    added conditionalExpression support

--- toAnalyse/test.ts
+++ toAnalyse/test.ts
@@ -1,15 +1,5 @@
 class Application {
     public void test(){
-        let a = z();
-        let b = y.z();
-        let c = x.y.z();
-        let d = w.x.y.z();
-        let e = y().z();
-        let f = x().y.z();
-        let g = x().y().z();
-        let h = this.z();
-        let i = this.y.z();
-        let j = Class.z();
-        let k = Class.y.z();
+        const a = a > 2 ? a : b;
     }
 }
