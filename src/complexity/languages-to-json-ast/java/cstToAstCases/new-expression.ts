ebf94788c807a4fb6fb42170f66c511ebc708a78
Alexandre Kueny <alexandre.kueny@altran.com>
2020-10-22 11:56:08 +0200

    skipped new-expression node, since its not in ts json

--- src/complexity/languages-to-json-ast/java/cstToAstCases/new-expression.ts
+++ src/complexity/languages-to-json-ast/java/cstToAstCases/new-expression.ts
@@ -6,15 +6,8 @@ import { NewExpressionChildren } from '../models/new-expression-children.model';
 export function run(cstNode: NewExpression, children: NewExpressionChildren): any {
     const unqualifiedClassInstanceCreationExpression = children.unqualifiedClassInstanceCreationExpression;
     const arrayCreationExpression = children.arrayCreationExpression;
-    
-    return {
-        kind: 'NewExpression',
-        start: cstNode.location.startOffset,
-        end: cstNode.location.endOffset,
-        pos: cstNode.location.startOffset,
-        children: [
-            ...[].concat(...unqualifiedClassInstanceCreationExpression?.map(e => cstToAst(e)) ?? []),
-            ...[].concat(...arrayCreationExpression?.map(e => cstToAst(e)) ?? [])
-        ]
-    }     
+    return [
+        ...[].concat(...unqualifiedClassInstanceCreationExpression?.map(e => cstToAst(e)) ?? []),
+        ...[].concat(...arrayCreationExpression?.map(e => cstToAst(e)) ?? [])
+    ]
 }
