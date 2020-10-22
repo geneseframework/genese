c22c91ef43eb1d99cb12efd93aef1a4361c07519
Alexandre Kueny <alexandre.kueny@altran.com>
2020-10-22 11:55:26 +0200

    added void keyword

--- src/complexity/languages-to-json-ast/java/cstToAstCases/result.ts
+++ src/complexity/languages-to-json-ast/java/cstToAstCases/result.ts
@@ -5,8 +5,10 @@ import { ResultChildren } from '../models/result-children.model';
 // @ts-ignore
 export function run(cstNode: Result, children: ResultChildren): any {
     const unannType = children.unannType;
+    const Void = children.Void;
 
     return [
-        ...[].concat(...unannType?.map(e => cstToAst(e)) || []),
+        ...[].concat(...unannType?.map(e => cstToAst(e)) ?? []),
+        ...[].concat(...Void?.map(e => cstToAst(e, 'void')) ?? [])
     ];
 }
