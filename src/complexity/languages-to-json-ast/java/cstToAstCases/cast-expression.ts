9d240de844038d7f4483ac27b713e1afe32280dd
guendouzbachir <guendouzbachir@hotmail.com>
2020-10-23 15:40:52 +0200

    Reference type cast expression (#156)
    * Cast expression
    
    * Cast expression
    
    Co-authored-by: Bachir GUENDOUZ <bachir.guendouz@altran.com>

--- src/complexity/languages-to-json-ast/java/cstToAstCases/cast-expression.ts
+++ src/complexity/languages-to-json-ast/java/cstToAstCases/cast-expression.ts
@@ -5,8 +5,10 @@ import { CastExpressionChildren } from '../models/cast-expression-children.model
 // @ts-ignore
 export function run(cstNode: CastExpression, children: CastExpressionChildren): any {
     const primitiveCastExpression = children.primitiveCastExpression;
-
+    const referenceTypeCastExpression = children.referenceTypeCastExpression;
+    
     return [
-        ...[].concat(...primitiveCastExpression?.map(e => cstToAst(e)) ?? [])
+        ...[].concat(...primitiveCastExpression?.map(e => cstToAst(e)) ?? []),
+        ...[].concat(...referenceTypeCastExpression?.map(e => cstToAst(e)) ?? [])
     ];
 }
