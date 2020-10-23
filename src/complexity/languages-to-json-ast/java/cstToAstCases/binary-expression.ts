749c4a9771bb863c9b2cc5b99dd640aa91cdcc73
Alexandre Kueny <alexandre.kueny@hotmail.com>
2020-10-23 16:28:33 +0200

    added many binOps (#158)
    Co-authored-by: Alexandre Kueny <alexandre.kueny@altran.com>

--- src/complexity/languages-to-json-ast/java/cstToAstCases/binary-expression.ts
+++ src/complexity/languages-to-json-ast/java/cstToAstCases/binary-expression.ts
@@ -43,10 +43,16 @@ function split(list) {
         return list[0];
     }
     const operatorsOrder = [
+        [SyntaxKind.EqualsToken, SyntaxKind.PlusEqualsToken, SyntaxKind.MinusEqualsToken, SyntaxKind.AsteriskEqualsToken, SyntaxKind.SlashEqualsToken, SyntaxKind.PercentEqualsToken, SyntaxKind.AmpersandEqualsToken, SyntaxKind.CaretEqualsToken, SyntaxKind.BarEqualsToken, SyntaxKind.LessThanLessThanEqualsToken, SyntaxKind.GreaterThanGreaterThanEqualsToken, SyntaxKind.GreaterThanGreaterThanGreaterThanEqualsToken],
+        [SyntaxKind.QuestionToken, SyntaxKind.ColonToken],
         [SyntaxKind.BarBarToken],
         [SyntaxKind.AmpersandAmpersandToken],
+        [SyntaxKind.BarToken],
+        [SyntaxKind.CaretToken],
+        [SyntaxKind.AmpersandToken],
         [SyntaxKind.EqualsEqualsToken, SyntaxKind.ExclamationEqualsToken],
         [SyntaxKind.LessThanToken, SyntaxKind.LessThanEqualsToken, SyntaxKind.GreaterThanToken, SyntaxKind.GreaterThanEqualsToken],
+        [SyntaxKind.LessThanLessThanToken, SyntaxKind.GreaterThanGreaterThanToken, SyntaxKind.GreaterThanGreaterThanGreaterThanToken],
         [SyntaxKind.PlusToken, SyntaxKind.MinusToken],
         [SyntaxKind.AsteriskToken, SyntaxKind.SlashToken, SyntaxKind.PercentToken],
     ];
