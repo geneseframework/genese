46ceee0fe5e2e99e4278f09a6653dd19d1304bc7
Alexandre Kueny <alexandre.kueny@altran.com>
2020-10-30 14:40:39 +0100

    fixed absolute path

--- index.ts
+++ index.ts
@@ -4,7 +4,8 @@ import { blueBright, red, yellowBright } from 'ansi-colors';
 import { exec } from 'child_process';
 import { Main } from './src/complexity/main';
 
-const { program } = require('commander');
+const {program} = require('commander');
+const pathNode = require('path');
 
 const pkg = require('./package.json');
 
@@ -13,51 +14,56 @@ const pkg = require('./package.json');
 // ---------------------------------------------------------------------------------------------------------
 
 // try {
-    console.log(yellowBright("WELCOME TO GENESE CLI"));
+console.log(yellowBright('WELCOME TO GENESE CLI'));
 
-    program.version(pkg.version)
-        .description('Genese cli');
+program.version(pkg.version)
+    .description('Genese cli');
 
-    // -------------------------------------   GENESE COMPLEXITY   ------------------------------------------
+// -------------------------------------   GENESE COMPLEXITY   ------------------------------------------
 
-    program
+program
     .option('-l, --language <language>', 'Language: js, ts, jsx, tsx or java', 'ts')
-    .option('-md, --markdown', 'Markdown type report')
+    .option('-md, --markdown', 'Markdown type report');
 
 
-    program.command('cpx [pathToAnalyse]')
-        .description('Calculates Complexity Index and cyclomatic complexity')
-        .action((pathFolderToAnalyze) => {
-            const path = process.cwd() + (pathFolderToAnalyze ? '/' + pathFolderToAnalyze : '');
-            const mainProcess = new Main();
+program.command('cpx [pathToAnalyse]')
+    .description('Calculates Complexity Index and cyclomatic complexity')
+    .action((pathFolderToAnalyze) => {
+        let path;
+        if (pathNode.isAbsolute(pathFolderToAnalyze)) {
+            path = pathFolderToAnalyze;
+        } else {
+            path = process.cwd() + (pathFolderToAnalyze ? '/' + pathFolderToAnalyze : '');
+        }
+        const mainProcess = new Main();
 
-            mainProcess.start(process.cwd(), path, __dirname, program.language, program.markdown)
+        mainProcess.start(process.cwd(), path, __dirname, program.language, program.markdown);
+    });
+
+// ----------------------------------   GENESE API for Angular   ----------------------------------------
+
+program.command('new <type>')
+    .description('New app | api')
+    .action(() => {
+        console.log(blueBright('STARTS GENESE API FOR ANGULAR APPS'));
+        const pathIndex = `node ${process.cwd()}/node_modules/genese-api-angular/index.js`;
+        exec(pathIndex, (error, stdout, stderr) => {
+            if (error) {
+                console.log(red(`Error in Genese cli execution : ${error.message}`));
+                return;
+            }
+            if (stderr) {
+                console.log(red(`Error in Genese cli command : ${stderr}`));
+                return;
+            }
+            if (stdout && stdout.length) {
+                console.log(yellowBright(`${stdout}`));
+            }
+            console.log(blueBright('API CREATED SUCCESSFULLY'));
         });
+    });
 
-    // ----------------------------------   GENESE API for Angular   ----------------------------------------
-
-    program.command('new <type>')
-        .description('New app | api')
-        .action(() => {
-            console.log(blueBright("STARTS GENESE API FOR ANGULAR APPS"));
-            const pathIndex = `node ${process.cwd()}/node_modules/genese-api-angular/index.js`;
-            exec(pathIndex, (error, stdout, stderr) => {
-                if (error) {
-                    console.log(red(`Error in Genese cli execution : ${error.message}`));
-                    return;
-                }
-                if (stderr) {
-                    console.log(red(`Error in Genese cli command : ${stderr}`));
-                    return;
-                }
-                if (stdout && stdout.length ) {
-                    console.log(yellowBright(`${stdout}`));
-                }
-                console.log(blueBright("API CREATED SUCCESSFULLY"));
-            });
-        });
-
-    program.parse(process.argv);
+program.parse(process.argv);
 
 // } catch (err) {
 //     console.error(red(`Error in Genese cli process : ${err}`));
