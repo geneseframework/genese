bb632886f22af2c011e7893ab8365c7574fa664c
Alexandre Kueny <alexandre.kueny@altran.com>
2020-11-16 09:15:06 +0100

    removed log

--- dev.js
+++ dev.js
@@ -6,9 +6,6 @@ var ansi_colors_1 = require("ansi-colors");
 var main = require("./dist/src/complexity/main");
 
 try {
-    console.log(ansi_colors_1.yellowBright("WELCOME TO GENESE COMPLEXITY"));
-    console.log();
-    console.log(ansi_colors_1.yellowBright("JSON AST CREATION"));
     // -------------------------------------   GENESE COMPLEXITY   ------------------------------------------
     var mainProcess = new main.Main();
     const pathFolderToAnalyze = 'my-absolute-folder'
